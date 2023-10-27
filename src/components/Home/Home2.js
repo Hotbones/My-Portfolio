e346d98c9f6d1593b5560452494343f6df71dd66
Mr. Bones <105388226+Hotbones@users.noreply.github.com>
2023-10-27 10:16:35 -0300

    Update Home2.js

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -17,36 +17,35 @@ function Home2() {
               LET ME <span className="blue"> INTRODUCE </span> MYSELF
             </h1>
             <p className="home-about-body">
-            I fell in love with IT environment and I have at least learnt
-              something, I think… 🤷‍♂️
+            I have a deep passion for the IT environment, and I've certainly learned a lot along the way, or so I believe... 🤷‍♂️
               <br />
-              <br />I have worked in classics like
+              <br />My experience encompasses classics such as
               <i>
                 <b className="blue"> Python, Javascript and Typescript. </b>
               </i>
               <br />
               <br />
-              My field of Interest are quality&nbsp;
+              I specialize in quality&nbsp;
               <i>
-                <b className="blue">Web Technologies and Products </b> and
-                also in areas related to{" "}
+                <b className="blue">Web Technologies and Products, </b> and
+                with a keen interest in areas related{" "}
                 <b className="blue">
                   Blockchain.
                 </b>
               </i>
               <br />
               <br />
-              Whenever possible, I also apply my passion for developing products
-              with <b className="blue">Python</b> and
+              Furthermore, whenever the opportunity arises, I channel my enthusiasm into developing products using
+              <b className="blue">Python</b> and
               <i>
                 <b className="blue">
                   {" "}
-                  Modern Javascript Library and Frameworks
+                  Modern Javascript Libraries and Frameworks
                 </b>
               </i>
               &nbsp; like
               <i>
-                <b className="blue"> Cypress and Playwright</b>
+                <b className="blue"> Cypress and Playwright.</b>
               </i>
             </p>
           </Col>
